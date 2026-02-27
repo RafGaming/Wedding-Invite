d8dd7f1f54d2fefc962e33b42eebcaa57a46a874
Sketsi <62134147+RafGaming@users.noreply.github.com>
2026-02-28 00:05:01 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Wedding Invitation",
+  description: "Jethro & Francisca Wedding Invitation",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
