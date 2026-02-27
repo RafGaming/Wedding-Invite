99bf1dbbfa47da1fba4c9eadc54dde23c32b7409
Sketsi <62134147+RafGaming@users.noreply.github.com>
2026-02-28 01:20:30 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,35 +1,34 @@
-export default function Page() {
-  return (
-    <main>
-      {/* cinematic triple parallax background */}
-      <div className="parallax-bg">
-        <div className="bg-layer layer1"></div>
-        <div className="bg-layer layer2"></div>
-        <div className="bg-layer layer3"></div>
-      </div>
+import Envelope from "./components/Envelope"; 
+import Sparkles from "./components/Sparkles"; 
+import Petals from "./components/Petals"; 
+import CursorTrail from "./components/CursorTrail";
 
-      {/* drifting sparkles */}
-      <Sparkles />
+export default function Home() { return ( <main style={{ minHeight: "300vh", paddingTop: "80px" }}>
 
-      {/* falling gold petals */}
-      <Petals />
-
-      {/* cursor gold dust trail */}
-      <CursorTrail />
-
-      {/* floating envelope */}
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "160px",
-          marginBottom: "200px",
-          position: "relative",
-          zIndex: 10
-        }}
-      >
-        <Envelope />
-      </div>
-    </main>
-  );
+  {/* cinematic triple parallax background */}
+  <div className="parallax-bg">
+    <div className="bg-layer layer1"></div>
+    <div className="bg-layer layer2"></div>
+  </div>
+  {/* drifting sparkles */}
+  <Sparkles />
+  {/* falling gold petals */}
+  <Petals />
+  {/* cursor gold dust trail */}
+  <CursorTrail />
+  {/* floating envelope */}
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      marginTop: "160px",
+      marginBottom: "200px",
+      position: "relative",
+      zIndex: 10
+    }}
+  >
+    <Envelope />
+  </div>
+</main>
+); 
 }
