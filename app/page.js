fdf7e8a478e5c8da306dd0c45ad17cdd79d48407
Sketsi <62134147+RafGaming@users.noreply.github.com>
2026-02-28 01:08:20 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,30 +1,35 @@
-  {/* cinematic triple parallax background */}
-  <div className="parallax-bg">
-    <div className="bg-layer layer1"></div>
-    <div className="bg-layer layer2"></div>
-  </div>
+export default function Page() {
+  return (
+    <main>
+      {/* cinematic triple parallax background */}
+      <div className="parallax-bg">
+        <div className="bg-layer layer1"></div>
+        <div className="bg-layer layer2"></div>
+        <div className="bg-layer layer3"></div>
+      </div>
 
-  {/* drifting sparkles */}
-  <Sparkles />
+      {/* drifting sparkles */}
+      <Sparkles />
 
-  {/* falling gold petals */}
-  <Petals />
+      {/* falling gold petals */}
+      <Petals />
 
-  {/* cursor gold dust trail */}
-  <CursorTrail />
+      {/* cursor gold dust trail */}
+      <CursorTrail />
 
-  {/* floating envelope */}
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      marginTop: "160px",
-      marginBottom: "200px",
-      position: "relative",
-      zIndex: 10
-    }}
-  >
-    <Envelope />
-  </div>
-
-</main>
+      {/* floating envelope */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "160px",
+          marginBottom: "200px",
+          position: "relative",
+          zIndex: 10
+        }}
+      >
+        <Envelope />
+      </div>
+    </main>
+  );
+}
